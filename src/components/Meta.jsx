3195968495c0849a5cba7aa222d0b0733298ad45
Park Inkook <pkook7@gmail.com>
2024-01-16 13:13:39 +0900

    라우터수정

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -4,10 +4,9 @@ const Meta = () => {
 
   return (
     <Helmet>
-      <meta property="og:type" content="website" />
-      <meta property="og:description" content="박인국 포트폴리오" />
-      <meta property="og:image" content="./images/박인국프로필.jpg" />
-      <title>소개합니다. 프론트엔드 개발자 박인국</title>
+      <meta property="og:type" content="website" data-react-helmet="true"/>
+      <meta property="og:description" content="박인국 포트폴리오" data-react-helmet="true"/>
+      <meta property="og:image" content="./images/박인국프로필.jpg" data-react-helmet="true"/>
     </Helmet>
   );
 };
--- src/index.js
+++ src/index.js
@@ -9,17 +9,9 @@ import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <HelmetProvider>
       <GlobalFonts />
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </HelmetProvider>
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
